9fb506205ba64bd2ad963a9467bfe16b84d92140
Source Academy User <No public email provided>
2022-09-21 14:51:32 +0800

    Changes made from Source Academy

---
+++ Mastery Check_1.js
@@ -0,0 +1,29 @@
+//Sum of first 'n' Natural Numbers
+
+
+//Recursive Version of the code
+function sum(n){
+    if (n === 1 ){
+        return 1;
+    }
+    
+    else {
+        return n + sum(n - 1);
+    }
+}
+
+
+
+//Iterative Version of the Code
+function sum_iter(n){
+    function helper(m, result){
+        return m > n 
+        ? result
+        : helper(m + 1, result + m);
+    }
+    
+return helper(1,0);
+
+}
+
+
