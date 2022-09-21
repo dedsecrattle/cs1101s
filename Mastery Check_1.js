a85b855a86e2269e73bee5a447f083e0befa26de
Source Academy User <No public email provided>
2022-09-21 15:16:44 +0800

    Changes made from Source Academy

--- Mastery Check_1.js
+++ Mastery Check_1.js
@@ -18,12 +18,38 @@ function sum(n){
 function sum_iter(n){
     function helper(m, result){
         return m > n 
-        ? result
-        : helper(m + 1, result + m);
-    }
-    
-return helper(1,0);
-
+                ? result
+                    : helper(m + 1, result + m);
 }
+    return helper(1,0);
+}
+
+//Recursive Version of the Code
+function factorial(n){
+    if (n===1){
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+//Iterative Version of the Code
+function factorial_iter(n){
+    function helper(m,result){
+        return m > n
+                ? result
+                    : helper(m+1, result * m);
+    }
+    return helper(1,1); 
+}
+
+factorial_iter(5);
+
+
+
+
+
+
 
 
