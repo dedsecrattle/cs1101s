0652320ad36d6eb83595a16a44a0e6b97b2f4db8
Source Academy User <Prabhat Kumar>
2022-11-01 21:55:41 +0800

    Changes made from Source Academy

--- PA_2016-17.js
+++ PA_2016-17.js
@@ -44,12 +44,14 @@ function gene_start(xs){
 }
 //Q1-F
 
+
+function gene_end(xs){
+    
+let result = null;
 function helper(xs){
     return (head(xs) === "G" || head(xs) === "A") && (head(tail(xs)) === "G"
         || head(tail(xs)) === "A");
 }
-function gene_end(xs){
-let result = null;
     function helper_2(xs){
     if(is_null(xs)){
         return null;
@@ -82,6 +84,87 @@ let result = null;
 return helper_2(xs);
 
 }
+//Q1-G
+function termination(xs){
+let result = null;
+function helper(xs){
+    return (head(xs) === "G" || head(xs) === "A") && (head(tail(xs)) === "G"
+        || head(tail(xs)) === "A");
+}
+    function helper_2(xs){
+    if(is_null(xs)){
+        return null;
+    }
+    else if(head(xs) === "T"){
+    if(is_null(tail(xs)) || is_null(tail(tail(xs)))){
+       return null;
+    }
+    else{
+         if(head(tail(xs)) === "G" && head(tail(tail(xs))) === "G"){
+            result = pair(head(xs),result);
+            return helper_2(tail(xs));
+        }
+        else if(helper(tail(xs))){
+            return pair(reverse(result),tail(tail(tail(xs))));
+        }
+        else{
+            result = pair(head(xs),result);
+            return helper_2(tail(xs));
+        }
+    }
+    }
+
+    else{
+        result = pair(head(xs),result);
+        return helper_2(tail(xs));
+    }
+
+}
+return helper_2(xs);
+}
+
+function start(xs){
+
+function is_start(xs){
+    if(is_null(tail(xs)) || is_null(tail(tail(xs)))){
+        return false;
+ }
+    else{
+        
+    return head(xs)==="A"&& head(tail(xs))=== "T" &&
+        head(tail(tail(xs))) === "G";
+}
+}
+    if(is_null(xs)){
+        return null;
+    }
+    else if(is_start(xs)){
+        return tail(tail(tail((xs))));
+    }
+    else{
+        return start(tail(xs));
+    }
+}
+    
+function all_genes(xs){
+    let result = null;
+    function solver(xs){
+        const start_list = start(xs);
+    const end = termination(start_list);
+    if(is_null(end)){
+        return reverse(result);
+    }
+    else{
+    result = pair(head(end),result);
+    return solver(tail(end));
+
+
+}
+    }
+    return solver(xs);
+    
+}
+
 
 
 
